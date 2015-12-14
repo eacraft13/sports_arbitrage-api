5093d788a7077274b932d4399e0f1c8670243658
Eric Craft <eacraft13@gmail.com>
2015-12-13 19:08:35 -0800

    Matches @show

--- test/matches.js
+++ test/matches.js
@@ -39,14 +39,29 @@ describe('matches', function() {
             .post('/matches')
             .send({
                 sport: 'NFL',
-                teams: {
-                    away: 'Oakland Raiders',
-                    home: 'Denver Broncos'
-                },
+                teams: { away: 'Oakland Raiders', home: 'Denver Broncos' },
                 time: 1450002600
             })
             .expect('Content-Type', /json/)
             .expect(201, done);
+        });
+    });
+
+
+
+    describe('@show', function() {
+        it('should return 404 with nonexistant match', function(done) {
+            request(app)
+            .get('/matches/nfl/tenessee-titans/new-england-patriots/1450002600')
+            .expect('Content-Type', /json/)
+            .expect(404, done);
+        });
+
+        it('should return 200', function(done) {
+            request(app)
+            .get('/matches/nfl/oakland-raiders/denver-broncos/1450002600')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
         });
     });
 
